b51046f247e157468180928f26474d7eeec97485
RainbowWebmaster <webmaster@rainbow-preschool.de>
2025-08-16 22:29:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
-function toggleHamburgerMenu() {
-  const headerNavigationLinksElement = document.getElementById('header-navigation-links');
+const navButton = document.getElementById("hamburgerMenu");
+const disclosure = document.getElementById("primaryNav");
+const listItems = disclosure.querySelectorAll("li a");
 
-  if (headerNavigationLinksElement && headerNavigationLinksElement.parentNode) {
-    headerNavigationLinksElement.parentNode.classList.toggle('hamburger-menu-expanded');
-  }
-
-  event.stopPropagation();
+function openNavigation() {
+  navButton.setAttribute("aria-expanded", "true");
+  disclosure.classList.remove("hidden");
 }
 
-function closeHamburgerMenu() {
-  const headerNavigationLinksElement = document.getElementById('header-navigation-links');
+function closeNavigation() {
+  navButton.setAttribute("aria-expanded", "false");
+  disclosure.classList.add("hidden");
+}
 
-  if (headerNavigationLinksElement && headerNavigationLinksElement.parentNode) {
-    headerNavigationLinksElement.parentNode.classList.remove('hamburger-menu-expanded');
+function toggleNavigation() {
+  const open = navButton.getAttribute("aria-expanded");
+  open === "false" ? openNavigation() : closeNavigation();
+}
+
+// This function closes an open disclosure if a user tabs away from the last anchor element in the list. It is reliant on the ul container having a class to check whether the relatedTarget is within the disclosure. If not, it will close.
+function handleBlur() {
+  const navList = event.currentTarget.closest(".primaryNavList");
+  if (!event.relatedTarget || !navList.contains(event.relatedTarget)) {
+    closeNavigation();
   }
 }
 
-(function () {
-  const bodyContentElement = document.getElementById('content');
-  if (bodyContentElement) {
-    bodyContentElement.addEventListener(
-      "click",
-      closeHamburgerMenu,
-      false,
-    );
+navButton.addEventListener("click", toggleNavigation);
+
+// add event to the last item in the nav list to trigger the disclosure to close if the user tabs out of the disclosure
+listItems[listItems.length - 1].addEventListener("blur", handleBlur);
+
+// Close the disclosure if a user presses the escape key
+window.addEventListener("keyup", (e) => {
+  if (e.key === "Escape") {
+    navButton.focus();
+    closeNavigation();
   }
-  
-  const headerNavigationLinksElement = document.getElementById('header-navigation-links');
-  if (headerNavigationLinksElement) {
-    headerNavigationLinksElement.addEventListener(
-      "click",
-      function () {
-        event.stopPropagation();
-      },
-      false,
-    );
-  }
-})();
+});
