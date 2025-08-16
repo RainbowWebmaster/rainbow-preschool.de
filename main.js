19180eca8e6ca9ffa13dc9662c280601e31fe467
RainbowWebmaster <webmaster@rainbow-preschool.de>
2025-08-16 20:44:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function toggleHamburgerMenu() {
-  var headerNavigationLinksElement = document.getElementById('header-navigation-links');
+  const headerNavigationLinksElement = document.getElementById('header-navigation-links');
 
   if (headerNavigationLinksElement && headerNavigationLinksElement.parentNode) {
     headerNavigationLinksElement.parentNode.classList.toggle('hamburger-menu-expanded');
@@ -9,9 +9,31 @@ function toggleHamburgerMenu() {
 }
 
 function closeHamburgerMenu() {
-  var headerNavigationLinksElement = document.getElementById('header-navigation-links');
+  const headerNavigationLinksElement = document.getElementById('header-navigation-links');
 
   if (headerNavigationLinksElement && headerNavigationLinksElement.parentNode) {
     headerNavigationLinksElement.parentNode.classList.remove('hamburger-menu-expanded');
   }
 }
+
+(function () {
+  const bodyContentElement = document.getElementById('content');
+  if (bodyContentElement) {
+    bodyContentElement.addEventListener(
+      "click",
+      closeHamburgerMenu,
+      false,
+    );
+  }
+  
+  const headerNavigationLinksElement = document.getElementById('header-navigation-links');
+  if (headerNavigationLinksElement) {
+    headerNavigationLinksElement.addEventListener(
+      "click",
+      function () {
+        event.stopPropagation();
+      },
+      false,
+    );
+  }
+})();
